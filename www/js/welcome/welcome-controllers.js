9b213d9350255e6fb50d8de6228559e12b7ad2eb
aliblackwell <aliblackwell@gmail.com>
2016-01-24 13:14:13 +0000

    changes

--- www/js/welcome/welcome-controllers.js
+++ www/js/welcome/welcome-controllers.js
@@ -2,23 +2,21 @@
 
 angular.module('david.welcomeControllers', [])
 
-  .controller('welcomeTabsCtrl', ['$scope', '$timeout', '$ionicTabsDelegate', '$state', function ($scope, $timeout, $ionicTabsDelegate, $state){
-    $timeout(function() {
-      $ionicTabsDelegate.select(1);
-    }, 20);
-  }])
 
   .controller('welcomeEpoqCtrl', ['$scope', '$timeout', '$ionicTabsDelegate', '$state', function ($scope, $timeout, $ionicTabsDelegate, $state){
 
   }])
 
 
-  .controller('welcomeCtrl', ['$scope', 'User', 'AvailablePerformances', '$location', 'Settings', '$rootScope', function ($scope, User, AvailablePerformances, $location, Settings, $rootScope){
+  .controller('welcomeCtrl', ['$scope', 'User', 'AvailablePerformances', '$location', 'Settings', '$rootScope', '$timeout', function ($scope, User, AvailablePerformances, $location, Settings, $rootScope, $timeout){
 
     var user, shows;
 
     // Workaround for ion-content bug
     $scope.d = {};
+
+    $scope.d.saveState = 'Save';
+    $scope.d.buttonStyle = 'button-positive'
 
     // Load available performances
     shows = AvailablePerformances;
@@ -26,11 +24,27 @@ angular.module('david.welcomeControllers', [])
       $scope.d.shows = shows;
       user = User;
       user.$bindTo($scope, 'user');
+      if (user.name) {
+        $scope.saveResponse();
+      }
     })
 
     $scope.setShow = function() {
       localStorage.setItem('showId', $scope.user.show);
       $rootScope.watchSettings();
+      $scope.resetSave();
+    }
+
+    $scope.saveResponse = function() {
+      $scope.d.saveState = 'Saved';
+      $scope.d.buttonStyle = 'button-balanced';
+      $scope.d.buttonIcon = ' ';
+    }
+
+    $scope.resetSave = function() {
+      $scope.d.saveState = 'Save';
+      $scope.d.buttonStyle = 'button-positive';
+      $scope.d.buttonIcon = '';
     }
 
   }])
