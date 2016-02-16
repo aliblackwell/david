fd8e401a9f74b3069ec4deb3ef722ed9d373b78e
aliblackwell <aliblackwell@gmail.com>
2016-02-16 20:13:31 +0000

    Disable swipe and show epoq

--- www/scenes/davidtolife/davidtolife-controllers.js
+++ www/scenes/davidtolife/davidtolife-controllers.js
@@ -1,57 +1,57 @@
 'use strict';
 
 angular.module('david.davidtolife', [])
-  .controller('davidtolifeCtrl', ['$scope', 'LifeSwipes', 'User', 'FinishedSwipes', function ($scope, LifeSwipes, User, FinishedSwipes){
+  .controller('davidtolifeCtrl', ['$scope', 'User', 'FinishedSwipes', '$ionicSlideBoxDelegate', function ($scope, User, FinishedSwipes, $ionicSlideBoxDelegate){
 
     var lifeSwipes,
-        counter = 0,
-        cardTypes = [
-      { image: '/img/davids/d1.jpg' },
-      { image: '/img/davids/d2.jpg' },
-      { image: '/img/davids/d3.jpg' },
-      { image: '/img/davids/d4.jpg' }
-    ];
+        counter = 0;
 
-    var setFinishedSwiping = function() {
-      var finished = new FinishedSwipes();
+    $scope.d = {}
 
-      finished.$add(user);
-    }
+    $scope.d.slideactive = true;
+    $scope.d.show = true;
+
+    $scope.turnImages = [
+      'DT1.JPG',
+      'DT2.JPG',
+      // 'DT3.JPG',
+      // 'DT4.JPG',
+      // 'DT5.JPG',
+      // 'DT6.JPG',
+      // 'DT7.JPG',
+      // 'DT8.JPG',
+      // 'DT9.JPG',
+      // 'DT10.JPG',
+      // 'DT11.JPG',
+      // 'DT12.JPG',
+      // 'DT13.JPG',
+      'DT14.JPG',
+      'DT15.JPG'
+    ]
+
+    console.log($scope.turnImages.length)
+
 
     var user = new User();
 
     user.$loaded().then(function(){
       user.uuid = user.$id;
-      lifeSwipes = new LifeSwipes(user);
-      $scope.cards = Array.prototype.slice.call(cardTypes, 0);
-    })
+    });
 
-
-
-
-
-    $scope.cardDestroyed = function(index) {
-      $scope.cards.splice(index, 1);
-    };
-
-    $scope.cardSwiped = function(sentiment, card) {
-      counter++;
-      card.sentiment = sentiment;
-      lifeSwipes.$add(card);
-      lifeSwipes.$save();
-      if (counter == cardTypes.length) {
-        $scope.epoq = true;
-        setFinishedSwiping();
+    $scope.slideHasChanged = function(index) {
+      console.log(index)
+      if (index === $scope.turnImages.length) {
+        $scope.setFinishedSwiping();
       }
     }
 
-    $scope.cardSwipedLeft = function(card) {
-      $scope.cardSwiped('dislike', card);
+    $scope.setFinishedSwiping = function() {
+      var finished = new FinishedSwipes();
+      finished.$add(user.name);
+      $scope.d.slideactive = false;
+      $ionicSlideBoxDelegate.enableSlide(false);
 
-    };
-    $scope.cardSwipedRight = function(card) {
-      $scope.cardSwiped('like', card);
-    };
+    }
 
 
   }])
