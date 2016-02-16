f431c9814fdb34b1a01e78737d594016ce2e51fe
aliblackwell <aliblackwell@gmail.com>
2016-02-16 20:12:22 +0000

    Welcome with tinder

--- www/scenes/welcome/welcome-controllers.js
+++ www/scenes/welcome/welcome-controllers.js
@@ -2,12 +2,14 @@
 
 angular.module('david.welcome', [])
 
-  .controller('welcomeCtrl', ['$scope', 'User', 'AvailablePerformances', '$location', 'Settings', '$rootScope', '$timeout', function ($scope, User, AvailablePerformances, $location, Settings, $rootScope, $timeout){
+  .controller('welcomeCtrl', ['$scope', 'User', 'AvailablePerformances', '$location', 'Settings', '$rootScope', '$timeout', 'LifeSwipes', 'FinishedSwipes', function ($scope, User, AvailablePerformances, $location, Settings, $rootScope, $timeout, LifeSwipes, FinishedSwipes){
 
     var user, shows;
 
     // Workaround for ion-content bug
     $scope.d = {};
+
+    $scope.d.formFilled = false;
 
     var loadAvailableShows = function() {
       shows = new AvailablePerformances();
@@ -22,9 +24,11 @@ angular.module('david.welcome', [])
     var loadUser = function() {
       user = new User();
       user.$bindTo($scope, 'user');
-      if (user.name) {
-        $scope.saveResponse();
-      }
+      user.$loaded().then(function() {
+        if (user.name) {
+          $scope.saveResponse();
+        }
+      })
 
     }
 
@@ -37,8 +41,16 @@ angular.module('david.welcome', [])
     }
 
     $scope.saveResponse = function() {
-      $scope.d.saveState = 'Saved';
-      $scope.d.buttonStyle = 'button-balanced';
+      if ($scope.d.formFilled) {
+        $scope.d.saveState = 'Save';
+        $scope.d.buttonStyle = 'button-positive';
+        $scope.d.formFilled = false;
+      } else {
+        $scope.d.saveState = 'Edit';
+        $scope.d.buttonStyle = 'button-balanced';
+        $scope.d.formFilled = true;
+      }
+
     }
 
     $scope.resetSave = function() {
@@ -47,6 +59,54 @@ angular.module('david.welcome', [])
     }
 
     loadAvailableShows();
+
+
+    var lifeSwipes,
+        counter = 0,
+        cardTypes = [
+      { image: '/img/davids/d1.jpg' },
+      { image: '/img/davids/d2.jpg' },
+      { image: '/img/davids/d3.jpg' },
+      { image: '/img/davids/d4.jpg' }
+    ];
+
+    var setFinishedSwiping = function() {
+      var finished = new FinishedSwipes();
+
+      finished.$add(user);
+    }
+
+    var user = new User();
+
+    user.$loaded().then(function(){
+      user.uuid = user.$id;
+      lifeSwipes = new LifeSwipes(user);
+      $scope.cards = Array.prototype.slice.call(cardTypes, 0);
+    })
+
+
+
+
+
+    $scope.cardDestroyed = function(index) {
+      $scope.cards.splice(index, 1);
+    };
+
+    $scope.cardSwiped = function(sentiment, card) {
+      counter++;
+      card.sentiment = sentiment;
+      lifeSwipes.$add(card);
+      lifeSwipes.$save();
+    }
+
+    $scope.cardSwipedLeft = function(card) {
+      $scope.cardSwiped('dislike', card);
+
+    };
+    $scope.cardSwipedRight = function(card) {
+      $scope.cardSwiped('like', card);
+    };
+
 
     // Should be run whenever this view is closed
     $scope.$on('$destroy', function() {
@@ -57,5 +117,6 @@ angular.module('david.welcome', [])
         user.$destroy();
       }
     });
+
 
   }])
