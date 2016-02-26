063697c583c6a056af8bf58fd8d78b40f32ec884
aliblackwell <aliblackwell@gmail.com>
2016-02-26 01:05:18 +0000

    rit bopping

--- www/scenes/religioninatie/religioninatie-controller.js
+++ www/scenes/religioninatie/religioninatie-controller.js
@@ -3,8 +3,14 @@
 angular.module('david.religioninatie', [])
   .controller('religioninatieCtrl', ['$scope', function ($scope){
 
+    console.log('going')
+    ripplyScott.init('js-ripple-btn', 0.75);
 
 
+    // Should be run whenever this view is closed
+    $scope.$on('$destroy', function() {
+      ripplyScott.destroy();
+    });
 
 
   }])
